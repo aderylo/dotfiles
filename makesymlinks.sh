ece5a0e7610edd2e94fc8494833825bedb0d6124
aderylo <aderylo>
2024-03-24 09:14:16 +0000

    add function to install zsh plugs

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -6,9 +6,9 @@
 
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources"    # list of files/folders to symlink in homedir
+dir=~/dotfiles                                                           # dotfiles directory
+olddir=~/dotfiles_old                                                    # old dotfiles backup directory
+files="bashrc vimrc vim zshrc oh-my-zsh private scrotwm.conf Xresources" # list of files/folders to symlink in homedir
 
 ##########
 
@@ -30,36 +30,66 @@ for file in $files; do
     ln -s $dir/$file ~/.$file
 done
 
-install_zsh () {
-# Test to see if zshell is installed.  If it is:
-if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
-    # Clone my oh-my-zsh repository from GitHub only if it isn't already present
-    if [[ ! -d $dir/oh-my-zsh/ ]]; then
-        git clone http://github.com/robbyrussell/oh-my-zsh.git
-    fi
-    # Set the default shell to zsh if it isn't currently set to zsh
-    if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
-        chsh -s $(which zsh)
-    fi
-else
-    # If zsh isn't installed, get the platform of the current machine
-    platform=$(uname);
-    # If the platform is Linux, try an apt-get to install zsh and then recurse
-    if [[ $platform == 'Linux' ]]; then
-        if [[ -f /etc/redhat-release ]]; then
-            sudo yum install zsh
-            install_zsh
+install_zsh() {
+    # Test to see if zshell is installed.  If it is:
+    if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+        # Clone my oh-my-zsh repository from GitHub only if it isn't already present
+        if [[ ! -d $dir/oh-my-zsh/ ]]; then
+            git clone http://github.com/robbyrussell/oh-my-zsh.git
         fi
-        if [[ -f /etc/debian_version ]]; then
-            sudo apt-get install zsh
-            install_zsh
+        # Set the default shell to zsh if it isn't currently set to zsh
+        if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+            chsh -s $(which zsh)
         fi
-    # If the platform is OS X, tell the user to install zsh :)
-    elif [[ $platform == 'Darwin' ]]; then
-        echo "Please install zsh, then re-run this script!"
-        exit
+    else
+        # If zsh isn't installed, get the platform of the current machine
+        platform=$(uname)
+        # If the platform is Linux, try an apt-get to install zsh and then recurse
+        if [[ $platform == 'Linux' ]]; then
+            if [[ -f /etc/redhat-release ]]; then
+                sudo yum install zsh
+                install_zsh
+            fi
+            if [[ -f /etc/debian_version ]]; then
+                sudo apt-get install zsh
+                install_zsh
+            fi
+        # If the platform is OS X, tell the user to install zsh :)
+        elif [[ $platform == 'Darwin' ]]; then
+            echo "Please install zsh, then re-run this script!"
+            exit
+        fi
     fi
-fi
+}
+
+install_zsh_plugs() {
+    # Use $ZSH_CUSTOM if set, otherwise default to ~/.oh-my-zsh/custom for plugin installations
+    local custom_dir=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+
+    # Ensure the custom plugins directory exists
+    mkdir -p "${custom_dir}/plugins"
+
+    # List of plugins to install
+    # Format: "plugin_name git_repository_url"
+    local plugins=(
+        "zsh-syntax-highlighting https://github.com/zsh-users/zsh-syntax-highlighting.git"
+        "zsh-autosuggestions https://github.com/zsh-users/zsh-autosuggestions.git"
+        # Add any other plugins here
+    )
+
+    for plugin_info in "${plugins[@]}"; do
+        # Split plugin_info into name and git repo URL
+        IFS=" " read -r plugin_name plugin_git_repo <<<"$plugin_info"
+
+        # Check if the plugin is already installed
+        if [[ ! -d "${custom_dir}/plugins/${plugin_name}" ]]; then
+            echo "Installing ${plugin_name}..."
+            git clone "${plugin_git_repo}" "${custom_dir}/plugins/${plugin_name}"
+        else
+            echo "${plugin_name} is already installed."
+        fi
+    done
 }
 
 install_zsh
+install_zsh_plugs
